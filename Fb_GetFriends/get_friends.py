d08c523f608d0f0e2b1acbb52bdb87ddc58316fe
Surachai <62043138+kapokcode@users.noreply.github.com>
2021-12-19 15:27:25 +0700

    Update get_friends.py

--- Fb_GetFriends/get_friends.py
+++ Fb_GetFriends/get_friends.py
@@ -14,33 +14,3 @@ def get_friends(token,limit):
     f.close()
 
 get_friends(token,10)
-
-
-
-
-
-
-# f = open("list.txt", "w")
-
-# for data in res.json()["data"]:
-#     f.write(data["id"] +" "+ data['name'] +"\n")
-
-# f.close()
-    
-
-# for data in r.json()['data']:
-#     print(data['name'] + " " + data['id'])
-
-# sub = requests.post('https://graph.facebook.com/100018342640179/subscribers?access_token=%s'%(token))
-# r = requests.post('https://graph.facebook.com/100018342640179/comments/?message=test&access_token=')
-# print(r.text)
-
-# r = requests.get('https://graph.facebook.com/100058118164596/friends?limit=20&access_token=%s'%(token)).json()["data"]
-# a = requests.get('https://graph.facebook.com/100018342640179/subscribers?limit=1&access_token=%s'%(token2))
-# print(a.text)
-
-# s = "https://graph.facebook.com/660888361199246/comments?method=POST&message=demo&access_token="+token2
-# cp_ttl = requests.get('https://graph.facebook.com/100024645752960?access_token=%s'%(token2)).json()['name']
-# r = requests.get(s)
-
-# print(cp_ttl)
